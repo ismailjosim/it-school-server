2c47525a2a8238c9d0b9c4c245f294ee34e856e3
JC DESIGN <ismailjosim99@gmail.com>
2022-10-26 05:58:39 +0600

    send data accourding to id

--- index.js
+++ index.js
@@ -16,27 +16,10 @@ app.get('/courses', (req, res) => {
     res.send(coursesData);
 })
 
-
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-
-    if (id === '08') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
-
-app.get('/news', (req, res) => {
-    res.send(news);
-})
-
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
+app.get('/course/:id', (req, res) => {
+	 const id = req.params.id;
+    const course = coursesData.find(n => n.id == id);
+    res.send(course);
 })
 
 
