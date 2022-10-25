2f4f43e608d787f7e7cdb75eca3c67b5c9edad77
JC DESIGN <ismailjosim99@gmail.com>
2022-10-25 20:26:05 +0600

    add course data functionality

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// CREATE API
+const coursesData = require('./data/courses-data.json');
+const news = require('./data/news.json');
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send(`API IS RUNNING!`);
-});
+    res.send("News API Running!");
+})
+
+app.get('/courses', (req, res) => {
+    res.send(coursesData);
+})
 
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
 
 
 app.listen(port, () => {
-    console.log(`Your Created Server Is Working On PORT: ${ port }`);
-
+    console.log(`News Server Running on Port: ${ port }`)
 })
