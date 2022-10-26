5e0bb4e7e8bfce9a0beca8d928e7375ed9abd334
JC DESIGN <ismailjosim99@gmail.com>
2022-10-26 20:33:31 +0600

    Modifiy configuration and index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const coursesData = require('./data/courses-data.json');
-const news = require('./data/news.json');
+
 
 app.use(cors());
 
@@ -18,7 +18,7 @@ app.get('/courses', (req, res) => {
 
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
-    const course = coursesData.find(n => n.id == id);
+    const course = coursesData.find(data => data.id == id);
     res.send(course);
 })
 
